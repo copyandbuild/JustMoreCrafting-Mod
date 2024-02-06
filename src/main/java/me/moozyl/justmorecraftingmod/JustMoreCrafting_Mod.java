17507c3d1c16632045f4baea28cec9dd29b71020
Veränderter <153541835+Veranderter@users.noreply.github.com>
2024-02-06 14:43:36 +0100

    Add files via upload

---
+++ src/main/java/me/moozyl/justmorecraftingmod/JustMoreCrafting_Mod.java
@@ -0,0 +1,23 @@
+package me.moozyl.justmorecraftingmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class JustMoreCrafting_Mod implements ModInitializer {
+
+    public static Logger LOGGER = LogManager.getLogger();
+
+    public static final String MOD_ID = "justmorecrafting-mod";
+
+    public static final String MOD_NAME = "JustMoreCrafting-Mod";
+
+    public void onInitialize() {
+        log(Level.INFO, "Initializing");
+    }
+
+    public static void log(Level level, String message) {
+        LOGGER.log(level, "[JustMoreCrafting] " + message);
+    }
+}
